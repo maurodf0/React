eed617c23e744278a6890c9e28b0c9f730603bd6
Mauro <maurodefalco96@gmail.com>
2024-04-15 00:53:13 +0200

    cleaning functions and eventlistener - useEffect

--- Counter/src/Card.jsx
+++ Counter/src/Card.jsx
@@ -2,12 +2,27 @@ import Count from "./Count";
 import ButtonContainer from "./ButtonContainer";
 import ResetBtn from "./ResetBtn";
 import Title from "./Title";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Card() {
+
+  //Use useState to keep track of things inside DOM/React
   const [count, setCount] = useState(0);
   const locked = count === 5 ? true : false;
+
+  //Use useEffect to track info outside DOM/React
+  useEffect(() => {
+    window.addEventListener('keydown', (e) => {
+      if(e.code === 'Space'){
+        setCount(count + 1)
+      }
+    });
+
+    return () => {} //cleaning function - Pulisce l'event listener precedente.
+  }, [count]); //dependencies array
+
+
 
   return (
     <div className={`card ${locked ? 'card--limit' : ''}`}>
