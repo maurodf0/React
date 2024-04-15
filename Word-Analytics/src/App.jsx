52317d27cdaf335bc1cf1c3a527b951d8924a737
Mauro <maurodefalco96@gmail.com>
2024-04-15 23:20:46 +0200

    Word Analytics Starter

---
+++ Word-Analytics/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Container from "./Container";
+
+function App() {
+
+  return (
+    <>
+   <Header />
+   <Container />
+   <Footer />
+   </>
+  )
+}
+
+export default App
