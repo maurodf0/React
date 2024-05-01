adbc4cf9152910230ab1ea5652d0785054beb21a
Mauro <maurodefalco96@gmail.com>
2024-05-01 19:00:25 +0200

    Inizialize TrekBag

---
+++ Trekbag/src/App.jsx
@@ -0,0 +1,17 @@
+import BackgroundHeading from "./components/BackgroundHeading"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <>
+    <BackgroundHeading />
+    <main>
+
+    </main>
+    < Footer />
+    </>
+  )
+}
+
+export default App
---
+++ Trekbag/src/components/BackgroundHeading.jsx
@@ -0,0 +1,7 @@
+export default function BackgroundHeading() {
+  return (
+    <div>
+      bg
+    </div>
+  )
+}
