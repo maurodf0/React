e85fb6bcbcc8f3d6ccf1ff0daaafac7bd29e6f19
Mauro <maurodefalco96@gmail.com>
2024-05-01 19:42:10 +0200

    Button and Map use

--- Trekbag/src/components/ButtonGroup.jsx
+++ Trekbag/src/components/ButtonGroup.jsx
@@ -1,12 +1,19 @@
-import Button from "./Button"
+import Button from "./Button";
+
+const secondaryButtonsText = [
+    "Mark all as complete", 
+    "Mark all as incomplete", 
+    "Reset to initial", 
+    "Remove all items"
+    ];
 
 export default function ButtonGroup() {
+
   return (
     <section className="button-group">
-     <Button type="secondary">Mark all as complete</Button>
-     <Button type="secondary">Mark all as incomplete</Button>
-     <Button type="secondary">Reset to initial</Button>
-     <Button type="secondary">Remove all items</Button>
+     {secondaryButtonsText.map(text => {
+        return <Button key="{text}" type="secondary">{text}</Button>
+     })}
     </section>
   )
 }
