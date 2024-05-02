8324dca10dd54ba2d9b1efd88b2c9009f7412a26
Mauro <maurodefalco96@gmail.com>
2024-05-02 22:48:04 +0200

    ending submit event

--- Trekbag/src/components/AddItemForm.jsx
+++ Trekbag/src/components/AddItemForm.jsx
@@ -7,26 +7,28 @@ export default function AddItemForm({setItems}) {
   // usiamo useRef per avere un riferimento di un elemento nel DOM
   // tipo document.querySelector
   const inputRef = useRef();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // Basic Validation
+    if(!itemText){
+      alert("Item can'be empty");
+      inputRef.current.focus();
+      return;
+    }
+
+    const newItem = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    }
+    setItems(prev => [...prev, newItem]);
+    setItemText("");
+  }
+
   return (
-    <form onSubmit={
-      (e) => {
-        e.preventDefault();
-
-        // Basic Validation
-        if(!itemText){
-          alert("Item can'be empty");
-          inputRef.current.focus();
-          return;
-        }
-
-        const newItem = {
-          id: new Date().getTime(),
-          name: itemText,
-          packed: false,
-        }
-        setItems(prev => [...prev, newItem]);
-        setItemText("");
-    }}>
+    <form onSubmit={handleSubmit}>
       <h2>Add an Item</h2>
       <input 
       //con l'attributo ref possiamo accedere a un elemento nel DOM
