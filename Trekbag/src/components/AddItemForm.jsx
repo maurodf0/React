aaa6f22230a98dd2456c425a7033359899f4bb82
Mauro <maurodefalco96@gmail.com>
2024-05-02 22:40:58 +0200

    Props elevating and Input interaction

--- Trekbag/src/components/AddItemForm.jsx
+++ Trekbag/src/components/AddItemForm.jsx
@@ -1,10 +1,33 @@
-import Button from "./Button.jsx"
+import Button from "./Button.jsx";
+import { useState } from "react";
 
-export default function AddItemForm() {
+export default function AddItemForm({setItems}) {
+  const [itemText, setItemText] = useState("");
   return (
-    <form>
+    <form onSubmit={
+      (e) => {
+        e.preventDefault();
+
+        // Basic Validation
+        if(!itemText){
+          alert("Item can'be empty");
+          return;
+
+        }
+
+        const newItem = {
+          id: new Date().getTime(),
+          name: itemText,
+          packed: false,
+        }
+        setItems(prev => [...prev, newItem]);
+        setItemText("");
+    }}>
       <h2>Add an Item</h2>
-      <input type="text"/>
+      <input 
+      autoFocus="true"
+      value={itemText} 
+      onChange={(e) => setItemText(e.target.value)}/>
       <Button>Add item to list</Button>
       </form>
   )
--- Trekbag/src/components/App.jsx
+++ Trekbag/src/components/App.jsx
@@ -1,18 +1,26 @@
+import { initialItems } from "../lib/constants"
 import BackgroundHeading from "./BackgroundHeading"
 import Footer from "./Footer"
 import Header from "./Header"
 import ItemList from "./ItemList"
 import Sidebar from "./Sidebar"
+import { useState } from "react"
 
 function App() {
+  const [items, setItems] = useState(initialItems);
 
   return (
     <>
     <BackgroundHeading />
     <main>
       <Header />
-      <ItemList />
-      < Sidebar />
+      {/* passiamo la funzione setItems e le props item 
+      dal component per gestirle in tutta l'app
+      passandole ad altri components (prendiamo 
+      i valori dai singoli component e li portiamo ad un lviello più alto 
+      nell'app) */}
+      <ItemList items={items} />
+      <Sidebar setItems={setItems}/>
     </main>
     < Footer />
     </>
