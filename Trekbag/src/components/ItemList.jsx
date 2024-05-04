0ad2d715d0f873c9c0f591c5fdd8699eba631ef1
Mauro <maurodefalco96@gmail.com>
2024-05-04 16:02:56 +0200

    Sorting with react-select and useState

--- Trekbag/src/components/ItemList.jsx
+++ Trekbag/src/components/ItemList.jsx
@@ -1,10 +1,49 @@
-import EmptyView from "./EmptyView"
+import EmptyView from "./EmptyView";
+import Select from "react-select";
+import {useState} from "react";
+
+const sortingOptions = [
+  {
+    value: "default", 
+    label: "Sort by default"
+},
+  {
+    value: "packed", 
+    label: "Sort by packed"
+  },
+  {
+    value: "unpacked", 
+    label: "Sort by unpacked"
+  },
+]
 
 export default function ItemList({items, handleRemoveItem, handleToggleItem}) {
+
+  const [sortBy, setSortBy] = useState('default');
+
+  const sortedItems = [...items].sort((a, b) => {
+    if(sortBy === 'packed') {
+      return b.packed - a.packed;
+    }
+    if(sortBy === 'unpacked') {
+      return a.packed - b.packed;
+    }
+
+    return;
+  })
+
   return (
     <ul className="item-list">
        {items.length === 0 ? <EmptyView /> : null}
-    {items.map(item => {
+
+    {items.length > 0 ? <section className="sorting">
+      <Select 
+        onChange={(option) => {setSortBy(option.value)}} 
+        options={sortingOptions} 
+        defaultValue={sortingOptions[0]} />
+    </section> : null/*items.map(item => <Item key={item.id} item={item} onRemoveItem={handleRemoveItem} onToggleItem={handleToggleItem}/>)*/}
+
+    {sortedItems.map(item => {
         return <Item  
           key={item.id} 
           item={item}  
