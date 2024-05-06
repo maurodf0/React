374976b92c4a26746d796997da43ff460f126a17
Mauro <maurodefalco96@gmail.com>
2024-05-07 00:11:10 +0200

    fix css import

--- CorpComment/src/App.tsx
+++ CorpComment/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+
       <h1>CorpComment</h1>
-    </>
+
   )
 }
 
