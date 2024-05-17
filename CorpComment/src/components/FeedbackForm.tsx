a103ae6f130764308cb001289a64ff766b970139
Mauro <maurodefalco96@gmail.com>
2024-05-18 00:51:49 +0200

    setText e text for textarea Validation

--- CorpComment/src/components/FeedbackForm.tsx
+++ CorpComment/src/components/FeedbackForm.tsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+import { MAX_CHARS } from "../lib/constants";
+
 export default function FeedbackForm() {
-  return <form className="form">
+
+  const [text, setText] = useState('');
+  const charsLeft = MAX_CHARS - text.length;
+
+  return (
+  <form className="form">
     <textarea 
+      value={text}
       id="feedback-textarea" 
       placeholder="Tricky" 
       spellCheck={false}
-      maxLength={150}
+      onChange={
+        (e) => setText(e.target.value)
+
+      }
     />
     <label htmlFor="feedback-textarea">
       Enter your feedback here, remember to #hashtag the company
     </label>
     <div>
-      <p className="u-italic">150</p>
+      <p className="u-italic">{charsLeft}</p>
       <button><span> SUBMIT </span></button>
     </div>
   </form>
+  );
 }
