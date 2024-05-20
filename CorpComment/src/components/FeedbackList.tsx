d943974bcdf461977ea4e0a92a95ea306cb2ba83
Mauro <maurodefalco96@gmail.com>
2024-05-21 00:53:17 +0200

    add spinnder while fetching

--- CorpComment/src/components/FeedbackList.tsx
+++ CorpComment/src/components/FeedbackList.tsx
@@ -1,37 +1,30 @@
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
 import { useEffect, useState } from "react";
 
 
 type Props = {}
 
-const exampleFeedbackItems = [
-  {
-    upvoteCount: 593,
-    badgeLetter: 'B',
-    companyName: 'ByteGrad',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis.',
-    daysAgo: 4
-  }, {
-    upvoteCount: 393,
-    badgeLetter: 'M',
-    companyName: 'MGrad',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis.',
-    daysAgo: 2
-  }];
-
 export default function FeedbackList({}: Props) {
   const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch('https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks')
       .then(res => res.json())
       .then(data => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
       })
   }, [])
 
   return (
     <ol className='feedback-list'>
+      {
+        isLoading ? <Spinner />  : null
+      }
+
       {
         feedbackItems.map(feedbackItem => {
           return  <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem}/>
