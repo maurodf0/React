b9470770e71f9f56889983c3f0ecdd6bdb6dd8d1
Mauro <maurodefalco96@gmail.com>
2024-05-21 00:23:44 +0200

    map over feedbackItems

--- CorpComment/src/components/FeedbackList.tsx
+++ CorpComment/src/components/FeedbackList.tsx
@@ -3,22 +3,20 @@ import FeedbackItem from "./FeedbackItem"
 
 type Props = {}
 
-const feedbackItem = {
-  upvoteCount: 593,
-  badgeLetter: 'B',
-  companyName: 'ByteGrad',
-  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis.',
-  daysAgo: 4
-}
-const feedbackItem2 = {
-  upvoteCount: 393,
-  badgeLetter: 'M',
-  companyName: 'MGrad',
-  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis.',
-  daysAgo: 2
-}
-
-const feedbackItems = [feedbackItem, feedbackItem2];
+const feedbackItems = [
+  {
+    upvoteCount: 593,
+    badgeLetter: 'B',
+    companyName: 'ByteGrad',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis.',
+    daysAgo: 4
+  }, {
+    upvoteCount: 393,
+    badgeLetter: 'M',
+    companyName: 'MGrad',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis.',
+    daysAgo: 2
+  }];
 
 export default function FeedbackList({}: Props) {
   return (
